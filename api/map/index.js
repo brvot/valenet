4ce95e311ba07c57e69e7e3f7eaf8a0a53f9f16a
CarlosVru <59267928+brvot@users.noreply.github.com>
2023-06-08 07:27:00 -0300

    modificacao html

--- api/map/index.js
+++ api/map/index.js
@@ -1,7 +1,9 @@
 module.exports = async function (context, req) {
+    
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
+    
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
@@ -10,4 +12,5 @@ module.exports = async function (context, req) {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
+
 }
