15dfe774bf8fe700334beb093e776e35e8df4d0a
CarlosVru <59267928+brvot@users.noreply.github.com>
2023-06-12 11:11:05 -0300

    implemented database connection

--- api/status/index.js
+++ api/status/index.js
@@ -1,6 +1,26 @@
+
 module.exports = async function (context, req) {
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: true
-    };
+
+    var mysql = require('mysql');
+
+    var con = mysql.createConnection({
+        host: "db-sokits.mysql.uhserver.com",
+        database: 'db_sokits',
+        user: "sokits",
+        password: "@Ajwog7eg2"
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+       
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Connected!"
+        };
+
+    });
+    
+
+    
+
 }
