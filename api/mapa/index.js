8255b7ba9ef856636cb1db3902c22bd0343104c4
CarlosVru <59267928+brvot@users.noreply.github.com>
2023-06-13 11:02:25 -0300

    Update index.js

--- api/mapa/index.js
+++ api/mapa/index.js
@@ -1,13 +1,24 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    
+    var mysql = require('mysql');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    var con = mysql.createConnection({
+        host: "db-sokits.mysql.uhserver.com",
+        database: 'db_sokits',
+        user: "sokits",
+        password: "@Ajwog7eg2"
+    });
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    con.connect(function(err) {
+        
+        if (err) throw err;
+       
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Connected!"
+        };
+
+    });
+
+
 }
